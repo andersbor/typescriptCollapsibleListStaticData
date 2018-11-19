a7edb4dcece27b7ca26724f65e475fbb615506ac
andersbor <anbo@easj.dk>
2018-11-19 11:30:51 +0100

    converted to TypeScript

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,14 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+let elements: HTMLCollectionOf<Element> = document.getElementsByClassName("collapsible");
+// let i: number;
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+for (let i: number = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function (): void {
+        this.classList.toggle("active");
+        var content: HTMLElement = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
